498d7cabe6ad960de219eae5b2bfc19736a1a599
Vipin Kumar <vipinkumar@Vipins-MacBook-Pro.local>
2025-03-06 10:46:31 +0530

    refactor: da input best practices

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -23,9 +23,9 @@ module "resource_group" {
 module "watsonx_governance" {
   source                  = "../../"
   region                  = var.region
-  plan                    = var.plan
+  plan                    = var.watsonx_governance_plan
   resource_group_id       = module.resource_group.resource_group_id
-  watsonx_governance_name = try("${local.prefix}-${var.name}", var.name)
+  watsonx_governance_name = try("${local.prefix}-${var.watsonx_governance_instance_name}", var.watsonx_governance_instance_name)
   access_tags             = var.access_tags
   resource_tags           = var.resource_tags
 }
