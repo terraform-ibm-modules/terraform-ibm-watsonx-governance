30787503848465bea5de99f11ad67a6ed362d92a
Zikra Iqbal <35029744+zikra-iqbal@users.noreply.github.com>
2025-12-17 14:58:58 +0530

    feat: Add the `Next Steps` for watsonx governance DA (#151)
    * feat: Added the next-step URL for watsonx.governance
    
    * chore: updated secondary_url link

--- solutions/fully-configurable/outputs.tf
+++ solutions/fully-configurable/outputs.tf
@@ -36,3 +36,28 @@ output "dashboard_url" {
   description = "The dashboard URL of the watsonx.governance instance."
   value       = module.watsonx_governance.dashboard_url
 }
+
+output "next_steps_text" {
+  value       = "Now, you can use the watsonx.governance to train, validate, tune and deploy AI models."
+  description = "Next steps text"
+}
+
+output "next_step_primary_label" {
+  value       = "Go to the IBM watsonx.governance dashboard"
+  description = "Primary label"
+}
+
+output "next_step_primary_url" {
+  value       = module.watsonx_governance.dashboard_url
+  description = "Primary URL"
+}
+
+output "next_step_secondary_label" {
+  value       = "Learn more about IBM watsonx.governance"
+  description = "Secondary label"
+}
+
+output "next_step_secondary_url" {
+  value       = "https://www.ibm.com/docs/en/watsonx/saas?topic=governing-ai"
+  description = "Secondary URL"
+}
