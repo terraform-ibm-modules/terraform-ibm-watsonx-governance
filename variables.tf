991ef01a22716f281b5656253a23e8b9217e0cd5
Md Anam Raihan <md.anam.raihan17@gmail.com>
2026-03-11 10:44:26 +0000

    readd validation

--- variables.tf
+++ variables.tf
@@ -44,6 +44,13 @@ variable "access_tags" {
   type        = list(string)
   description = "Add access management tags to the watsonx.governance instance to control access. [Learn more](https://cloud.ibm.com/docs/account?topic=account-tag&interface=ui#create-access-console)."
   default     = []
+  
+  validation {
+    condition = alltrue([
+      for tag in var.access_tags : can(regex("[\\w\\-_\\.]+:[\\w\\-_\\.]+", tag)) && length(tag) <= 128
+    ])
+    error_message = "Tags must match the regular expression `\"[\\w\\-_\\.]+:[\\w\\-_\\.]+\"`. [Learn more](https://cloud.ibm.com/docs/account?topic=account-tag&interface=ui#limits)."
+  }
 }
 
 variable "watsonx_governance_name" {
